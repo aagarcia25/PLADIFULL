112f1f42281d34d0f98d24a0e1d66e183fe9b685
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2024-03-11 13:05:22 -0600

    update

--- pladi/src/components/share/SimpleTable.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SimpleTable = () => {
-  return <div></div>;
-};
-
-export default SimpleTable;
--- pladi/src/services/localStorage.ts
+++
@@ -1,9 +0,0 @@
-//IDENTIFICAR QUE EL USUARIO YA ESTA LOGEADO
-export const setItem = (data: any, item: string) => {
-  localStorage.setItem(item, JSON.stringify(data));
-};
-export const getItem = (item: string): any => {
-  const value = localStorage.getItem(item);
- // console.log(value);
-  return value !== null ? String(value) : "";
-};
